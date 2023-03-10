f9f47e429546daa20e67adcd056fda547988c835
Dmitriy Karpushin <107479981+chilledsunset@users.noreply.github.com>
2023-03-10 16:48:14 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,61 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    PostController postController;
+    public static final String API_POSTS = "/api/posts";
+    public static final String API_POSTS_ID = "/api/posts/\\d+";
+    public static final String STR = "/";
+    public static final String GET_METHOD = "GET";
+    public static final String POST_METHOD = "POST";
+    public static final String DELETE_METHOD = "DELETE";
+
+    @Override
+    public void init() throws ServletException {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        postController = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse response) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+
+            if (method.equals(GET_METHOD) && path.equals(API_POSTS)) {
+                postController.all(response);
+                return;
+            }
+            if (method.equals(GET_METHOD) && path.matches(API_POSTS_ID)) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf(STR) + 1));
+                postController.getById(id, response);
+                return;
+            }
+            if (method.equals(POST_METHOD) && path.equals(API_POSTS)) {
+                postController.save(req.getReader(), response);
+            }
+            if (method.equals(DELETE_METHOD) && path.matches(API_POSTS_ID)) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf(STR) + 1));
+                postController.removeById(id, response);
+            }
+        } catch (NotFoundException e) {
+            e.getMessage();
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (IOException ioException) {
+            ioException.getMessage();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
